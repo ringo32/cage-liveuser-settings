1766eb2d8aab565d4fd3d3a9c26e7f8b4abc95e9
ringo de kroon <ringodekroon@gmail.com>
2021-12-03 23:01:53 +0100

    Initial commit

---
+++ .local/share/calamares-online/calamares_online.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo cp -praf /usr/share/calamares/settings.conf_online /usr/share/calamares/settings.conf
+sudo cp -praf /usr/share/calamares/modules/packages.conf_online /usr/share/calamares/modules/packages.conf
+sudo cp -praf /usr/share/calamares/modules/welcome.conf_online /usr/share/calamares/modules/welcome.conf
+
